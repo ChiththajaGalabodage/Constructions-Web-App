f91839e343b5c34238be6ac46aa18d698360325a
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-05-07 23:56:17 +0530

    implement about us and fix

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -1,67 +1,42 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+
+//import AboutImg from "../../assets/images/about-us.jpg";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import About from "./About";
 
 const Home = () => {
   return (
     <>
-      <header>
-        <div className="container py-3">
-          <Navbar expand="lg">
-            <Navbar.Brand href="#home" className="logo">
-              <span>UrbanEdge </span>
-              Constructions
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <Nav.Link href="#home" className="nav-link">
-                  Home
-                </Nav.Link>
-                <Nav.Link href="#link" className="nav-link">
-                  About Us
-                </Nav.Link>
-                <Nav.Link href="#link" className="nav-link">
-                  Services
-                </Nav.Link>
-                <Nav.Link href="#link" className="nav-link">
-                  Projects
-                </Nav.Link>
-                <Nav.Link href="#link" className="nav-link">
-                  Blogs
-                </Nav.Link>
-                <Nav.Link href="#link" className="nav-link">
-                  Contact Us
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      </header>
+      <Header />
       <main>
+        {/* Hero Sction*/}
         <section className="section-1">
           <div className="hero d-flex align-items-center">
-            <div className="cotainer-fluid">
+            <div className="container-fluid">
               <div className="text-center">
                 <span>Welcome Amazing Constructions</span>
                 <h1>
-                  Crafting dreams with <br />
-                  precision and excellence.
+                  Crafting dreams with <br /> precision & excellence
                 </h1>
                 <p>
                   We excel at transforming visions into reality through
-                  outstanding crafsmanship and precise
+                  outstanding craftsmanship and precise <br /> attention to
+                  detail. With years of experience and a dedication to quality.
                 </p>
                 <div className="mt-4">
-                  <a className="btn btn-primary">Contact Now</a>
-                  <a className="btn btn-secondary ms-2">View Projects</a>
+                  <a className="btn btn-primary large">Contact Now</a>
+                  <a className="btn btn-secondary ms-2 large">View projects</a>
                 </div>
               </div>
             </div>
           </div>
         </section>
+
+        {/* about us section */}
+        <About />
       </main>
-      <footer></footer>
+      <Footer />
     </>
   );
 };
