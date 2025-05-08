e88031bff939b1c141920b3bbdf118f261125644
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-05-08 10:29:25 +0530

    Why Choose Us  📝

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -5,6 +5,10 @@ import Header from "../common/Header";
 import Footer from "../common/Footer";
 import About from "./About";
 import ServiceImg from "../../assets/images/construction1.jpg";
+import ConstrutionImg from "../../assets/images/construction2.jpg";
+import Icon1 from "../../assets/images/icon-1.svg";
+import Icon2 from "../../assets/images/icon-2.svg";
+import Icon3 from "../../assets/images/icon-3.svg";
 
 const Home = () => {
   return (
@@ -145,6 +149,74 @@ const Home = () => {
           </div>
         </section>
 
+        {/*Why Choose Us*/}
+        <section className="section-4 py-5">
+          <div className="container py-5">
+            <div className="section-header text-center">
+              <span>Why Choose Us</span>
+              <h2>Discover our wide variety of projects.</h2>
+              <p>
+                Created in close partnership with our clients and collaborators,
+                this approach merges industry expertise, decades of experience,
+                <br />
+                innovation, and flexibility to consistently deliver excellence.
+              </p>
+            </div>
+            <div className="row pt-4">
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon1} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Cutting-Edge Solutions</h3>
+                  </div>
+
+                  <p>
+                    Small actions create big impacts. It all begins and ends
+                    with each employee committing to safer work practices daily,
+                    ensuring they return home safely.
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon2} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Cutting-Edge Solutions</h3>
+                  </div>
+
+                  <p>
+                    Small actions create big impacts. It all begins and ends
+                    with each employee committing to safer work practices daily,
+                    ensuring they return home safely.
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon3} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Cutting-Edge Solutions</h3>
+                  </div>
+
+                  <p>
+                    Small actions create big impacts. It all begins and ends
+                    with each employee committing to safer work practices daily,
+                    ensuring they return home safely.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
         {/* Our Projects */}
         <section className="section-3 bg-light py-5">
           <div className="container-fluid py-5">
@@ -160,11 +232,11 @@ const Home = () => {
               <div className="col-md-3 col-lg-3">
                 <div className="item">
                   <div className="service-image">
-                    <img src={ServiceImg} alt="" className="w-100" />
+                    <img src={ConstrutionImg} alt="" className="w-100" />
                   </div>
                   <div className="service-body">
                     <div className="service-title">
-                      <h3>Civil Construction</h3>
+                      <h3>Goa Project 2025</h3>
                     </div>
                     <div className="service-content">
                       <p>
@@ -183,11 +255,11 @@ const Home = () => {
               <div className="col-md-3 col-lg-3">
                 <div className="item">
                   <div className="service-image">
-                    <img src={ServiceImg} alt="" className="w-100" />
+                    <img src={ConstrutionImg} alt="" className="w-100" />
                   </div>
                   <div className="service-body">
                     <div className="service-title">
-                      <h3>Civil Construction</h3>
+                      <h3>Goa Project 2025</h3>
                     </div>
                     <div className="service-content">
                       <p>
@@ -206,11 +278,11 @@ const Home = () => {
               <div className="col-md-3 col-lg-3">
                 <div className="item">
                   <div className="service-image">
-                    <img src={ServiceImg} alt="" className="w-100" />
+                    <img src={ConstrutionImg} alt="" className="w-100" />
                   </div>
                   <div className="service-body">
                     <div className="service-title">
-                      <h3>Civil Construction</h3>
+                      <h3>Goa Project 2025</h3>
                     </div>
                     <div className="service-content">
                       <p>
@@ -229,11 +301,11 @@ const Home = () => {
               <div className="col-md-3 col-lg-3">
                 <div className="item">
                   <div className="service-image">
-                    <img src={ServiceImg} alt="" className="w-100" />
+                    <img src={ConstrutionImg} alt="" className="w-100" />
                   </div>
                   <div className="service-body">
                     <div className="service-title">
-                      <h3>Civil Construction</h3>
+                      <h3>Goa Project 2025</h3>
                     </div>
                     <div className="service-content">
                       <p>
